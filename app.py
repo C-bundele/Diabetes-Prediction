0ca3e56600c81afcd6c3e883f8ae748e846388ee
C-bundele <chanchalbundele04@gmail.com>
2024-12-20 22:01:22 -0500

    solved mixed data type error

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ st.title("Diabetes Prediction App")
 
 # Input fields for user data
 st.sidebar.header("Enter Patient Data")
-pregnancies = st.sidebar.number_input("Pregnancies", min_value=0, max_value=20, value=3)
-glucose = st.sidebar.number_input("Glucose Level", min_value=0, max_value=300, value=1)
-blood_pressure = st.sidebar.number_input("Blood Pressure", min_value=0, max_value=200, value=85)
-skin_thickness = st.sidebar.number_input("Skin Thickness", min_value=0, max_value=100, value=35)
-insulin = st.sidebar.number_input("Insulin Level", min_value=0, max_value=900, value=180)
-bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=100.0, value=0)
-dpf = st.sidebar.number_input("Diabetes Pedigree Function (DPF)", min_value=0.0, max_value=3.0, value=0.8)
-age = st.sidebar.number_input("Age", min_value=1, max_value=120, value=20)
+pregnancies = st.sidebar.number_input("Pregnancies", min_value=0, max_value=20, value=0)
+glucose = st.sidebar.number_input("Glucose Level", min_value=0.0, max_value=300.0, value=0.0)
+blood_pressure = st.sidebar.number_input("Blood Pressure", min_value=0.0, max_value=200.0, value=80.0)
+skin_thickness = st.sidebar.number_input("Skin Thickness", min_value=0.0, max_value=100.0, value=21.0)
+insulin = st.sidebar.number_input("Insulin Level", min_value=0.0, max_value=900.0, value=90.0)
+bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=100.0, value=0.0)
+dpf = st.sidebar.number_input("Diabetes Pedigree Function (DPF)", min_value=0.0, max_value=3.0, value=0.2)
+age = st.sidebar.number_input("Age", min_value=1.0, max_value=120.0, value=2.0)
 
 # Create user input as a dictionary
 user_input = {
